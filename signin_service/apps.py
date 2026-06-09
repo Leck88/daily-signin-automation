0506e82082f1534c5ac7ddf2cc6ebfa33fdd500a
Leck88 <80998848+Leck88@users.noreply.github.com>
2026-06-09 14:45:26 +0800

    Update concrete Alipay sign-in support: signin_service/apps.py

---
+++ signin_service/apps.py
@@ -0,0 +1,21 @@
+"""常用 App 包名。
+
+这里不写账号、密码、Cookie。脚本只控制手机上已经登录的 App。
+新增 App 时，优先把包名补到这里，再在 config 目录新增对应 YAML。
+"""
+
+# 支付宝
+ALIPAY = "com.eg.android.AlipayGphone"
+
+# 淘宝
+TAOBAO = "com.taobao.taobao"
+
+# 闲鱼
+IDLE_FISH = "com.taobao.idlefish"
+
+# 京东
+JD = "com.jingdong.app.mall"
+
+# 拼多多
+PINDUODUO = "com.xunmeng.pinduoduo"
+
